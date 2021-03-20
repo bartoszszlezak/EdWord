2e320c9305118585bfb17df93fad918762b9bf8d
bartoszszlezak <barmikszl@gmail.com>
2021-03-20 15:30:17 +0100

    Initial

---
+++ edword-spring/src/main/java/com/example/edwordspring/EdwordSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.edwordspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdwordSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdwordSpringApplication.class, args);
+    }
+
+}
---
+++ edword-spring/src/test/java/com/example/edwordspring/EdwordSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edwordspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdwordSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
