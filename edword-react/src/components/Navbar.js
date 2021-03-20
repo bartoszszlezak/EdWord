8f83cfbc4110afe62b6c3e5170b1f39e54f00123
bartoszszlezak <barmikszl@gmail.com>
2021-03-20 22:48:22 +0100

    Upgraded navbar

--- edword-react/src/components/Navbar.js
+++ edword-react/src/components/Navbar.js
@@ -1,47 +1,48 @@
 import React, {useState} from "react";
 import './styles/Navbar.css';
 import {Link} from "react-router-dom";
-import './styles/Navbar.css';
 
 function Navbar(){
 
     const [clicked, setState] = useState(false);
     const mobileMenuOff = () => setState(false);
     const changeClicked = () => setState(!clicked);
-    return(
-      <>
-          <nav className={'navbar'}>
-              <div className='nav-container'>
-                  <Link className={'nav-logo'} to={'/home'} onClick={mobileMenuOff}>
-                      EdWord <i className={"fas fa-book-reader"} />
-                  </Link>
-              </div>
-              <div className={'menu-icon'} onClick={changeClicked}>
-                  <i className={clicked ? 'fas fa-times' : 'fas fa-bars'} />
-              </div>
 
-              <ul className={clicked ? 'menu-mobile' : 'menu-desktop'}>
-                <li className={'nav-page'}>
-                    <Link className={'nav-page-link'} to={'/home'} onClick={mobileMenuOff}>
-                        Home
-                    </Link>
-                </li>
-                  <li className={'nav-page'}>
-                      <Link className={'nav-page-link'} to={'/learn'} onClick={mobileMenuOff}>
-                          Learn
-                      </Link>
-                  </li>
-                  <li className={'nav-page'}>
-                      <Link className={'nav-page-link'} to={'/revision'} onClick={mobileMenuOff}>
-                          Revision
-                      </Link>
-                  </li>
-                  <li className={'nav-page'}>
-                      <Link className={'nav-page-link-mobile'} to={'/signin'} onClick={mobileMenuOff}>
-                          Sign In
-                      </Link>
-                  </li>
-              </ul>
+    
+return(
+      <>
+          <nav className='navbar'>
+            <div className='nav-container'>
+                <Link className='nav-logo' to='/' onClick={mobileMenuOff}>
+                    EdWord <i className="fas fa-book-reader" />
+                </Link>
+                <div className='menu-icon' onClick={changeClicked}>
+                    <i className={clicked ? 'fas fa-times' : 'fas fa-bars'} />
+                </div>
+
+              <ul className={clicked ? 'menu-desktop active' : 'menu-desktop'}>
+                    <li className='nav-page'>
+                        <Link className='nav-page-link' to='/' onClick={mobileMenuOff}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className='nav-page'>
+                        <Link className='nav-page-link' to='/learn' onClick={mobileMenuOff}>
+                            Learn
+                        </Link>
+                    </li>
+                    <li className='nav-page'>
+                        <Link className='nav-page-link' to='/revision' onClick={mobileMenuOff}>
+                            Revision
+                        </Link>
+                    </li>
+                    <li className='nav-page'>
+                        <Link className='nav-page-link-mobile' to='/signin' onClick={mobileMenuOff}>
+                            Sign In
+                        </Link>
+                    </li>
+                </ul>
+            </div>
           </nav>
       </>
     );
