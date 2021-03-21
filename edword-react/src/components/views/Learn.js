c872fa3b13c06e2fe6ad391e3dd44aa9864889ac
bartoszszlezak <barmikszl@gmail.com>
2021-03-21 13:44:26 +0100

    Footer component created

---
+++ edword-react/src/components/views/Learn.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/LearnSection.css'
+
+
+function Learn() {
+    return (
+        <div className='learn-section'>
+            <h1>LEARN SECTION</h1>
+        </div>
+    );
+}
+
+export default Learn
