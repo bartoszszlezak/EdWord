5dbb0892130c715d218dfb6a9dc94a435783c403
bartoszszlezak <barmikszl@gmail.com>
2021-03-28 20:01:11 +0200

    Learn and revision views created

--- edword-react/src/components/LearnSection.js
+++ edword-react/src/components/LearnSection.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import './styles/LearnSection.css';
+import video from '../videos/video4.mp4';
 
 function LearnSection() {
+
+
     return (
         <div className='learn-section'>
-            <div className='flashcard'>
-                Elo
+            <video src={video} autoPlay loop muted />
+            <div className='flashcard' />
+            <div className='buttons-learn'>
+                <button>
+                    Next
+                </button>
+                <button>
+                    Done
+                </button>
             </div>
         </div>
-    )
+    );
 }
 
-export default LearnSection
+export default LearnSection;
+
+
