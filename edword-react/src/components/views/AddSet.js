3bc90dba1a1e4574043b4e5484bc3de3a4370368
bartoszszlezak <barmikszl@gmail.com>
2021-05-22 22:07:21 +0200

    Added stylization for forms

--- edword-react/src/components/views/AddSet.js
+++ edword-react/src/components/views/AddSet.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import SetForm from '../SetForm';
 import { Link } from 'react-router-dom';
-import video from '../../videos/video4.mp4';
+// import video from '../../videos/video4.mp4';
 import '../../App.css';
 
 
@@ -19,7 +19,7 @@ function AddSet() {
             <SetForm submitForm={submitSetForm} />
             ) : (
                 <div className='succes-signup'>
-                    <video src={video} autoPlay loop muted />
+                    {/* <video src={video} autoPlay loop muted /> */}
                     <Link to='/addword'>
                         <button>Your set was created succesfully</button>
                     </Link>
--- edword-react/src/components/views/AddWord.js
+++ edword-react/src/components/views/AddWord.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import WordForm from '../WordForm';
 import { Link } from 'react-router-dom';
-import video from '../../videos/video4.mp4';
+// import video from '../../videos/video4.mp4';
 import '../../App.css';
 
 function AddWord() {
@@ -18,7 +18,7 @@ function AddWord() {
             <WordForm submitForm={submitWordForm} />
             ) : (
                 <div className='succes-signup'>
-                    <video src={video} autoPlay loop muted />
+                    {/* <video src={video} autoPlay loop muted /> */}
                     <Link to='/sets'>
                         <button>Added words succesfully</button>
                     </Link>
